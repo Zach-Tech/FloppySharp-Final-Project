0ad522770ba304be14103ba6585ee27450fc38cf
Zachary Childers <zdchilders10@sccsc.edu>
2020-11-01 21:21:59 -0500

    Add project files.

---
+++ Admin Modules/AdminStuff.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZacharyChilders_Final_Project_CPT_185_FloppySharp.Admin_Modules
+{
+    [Name("Admin Commands")]
+    [RequireContext(ContextType.Guild)]
+    [RequireUserPermission(GuildPermission.ManageRoles)]
+    public class AdminStuff : ModuleBase<SocketCommandContext>
+    {
+        [Command("say"), Summary("Bot will repeat argument -> ")]
+        public async Task  Say([Remainder,Summary("User's words repeated by Floppy")] string say)
+        {
+            await ReplyAsync(say);
+            await Context.Message.DeleteAsync();
+        }
+
+    }
+}
---
+++ Modules/Commands.cs
@@ -0,0 +1,22 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZacharyChilders_Final_Project_CPT_185_FloppySharp.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        //Basic first command, I say "Ping", bot replies "Pong"
+        //Generally, all commands must be activated by a prefix,
+        //Unless the bot utilizes listeners and kwargs.
+        [Command("Ping")]
+        public async Task Ping() { await ReplyAsync("pong"); }
+
+
+
+
+    }
+}
